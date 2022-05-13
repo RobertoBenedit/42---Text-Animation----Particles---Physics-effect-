795f801b4224de1a1ac54f80d92d6b2bb292372a
RobertoBenedit <roberto.benedit.contador@gmail.com>
2022-05-13 15:05:01 -0300

    return to origin

--- app2.js
+++ app2.js
@@ -19,8 +19,8 @@ window.addEventListener("mousemove", (event) => {
 });
 
 window.addEventListener("touchmove", (event) => {
-    mouse.x = event.touches[ 0 ].clientX;
-    mouse.y = event.touches[ 0 ].clientY;
+    mouse.x = event.touches[0].clientX;
+    mouse.y = event.touches[0].clientY;
 });
 
 ctx.fillStyle = "#fff";
@@ -32,10 +32,10 @@ class Particle {
     constructor(x, y) {
         this.x = x;
         this.y = y;
-        this.size = 0.25;
+        this.size = 1;
         this.baseX = this.x;
         this.baseY = this.y;
-        this.density = Math.random() * 13 + 1;
+        this.density = Math.random() * 7 + 1;
     }
     draw() {
         ctx.fillStyle = "#fff";
@@ -45,23 +45,30 @@ class Particle {
         ctx.fill();
     }
     update() {
-        let dx = mouse.x - this.x; // distance in the x axis
-        let dy = mouse.y - this.y; // distance in the y axis
-        let distance = Math.sqrt(dx * dx + dy * dy); // pythagoras theorem
+        let dx = mouse.x - this.x;
+        let dy = mouse.y - this.y;
+        let distance = Math.sqrt(dx * dx + dy * dy);
 
-        let forceDirectionX = dx / distance; // this represent sin or cos? the answer is sin, cos is opposite
-        let forceDirectionY = dy / distance; // this represent sin or cos? the answer is sin, cos is opposite
+        let forceDirectionX = dx / distance;
+        let forceDirectionY = dy / distance;
 
-        let maxDistance = mouse.radius; // this is the radius of the circle
-        let force = (maxDistance - distance) / maxDistance; // this is the force of attraction
-        let forceX = forceDirectionX * force * this.density; // this is the force of attraction in the x direction
-        let forceY = forceDirectionY * force * this.density; // this is the force of attraction in the y direction
+        let maxDistance = mouse.radius;
+        let force = (maxDistance - distance) / maxDistance;
+        let directionX = forceDirectionX * force * this.density;
+        let directionY = forceDirectionY * force * this.density;
 
-        if (distance < mouse.radius) { // this is the radius of the circle
-            this.x += forceX; // this is the force of attraction in the x direction
-            this.y += forceY; // this is the force of attraction in the y direction
+        if (distance < mouse.radius) {
+            this.x += directionX;
+            this.y += directionY;
         } else {
-            this.size = 1;
+            if (this.x !== this.baseX) {
+                let dx = this.x - this.baseX;
+                this.x -= dx * 0.05;
+            }
+            if (this.y !== this.baseY) {
+                let dy = this.y - this.baseY;
+                this.y -= dy * 0.05;
+            }
         }
     }
 }
